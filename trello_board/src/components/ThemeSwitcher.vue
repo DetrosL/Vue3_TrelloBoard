fc49bc1a1ff7d6848d9d7c18338dcf4aaec114d6
Laiz <megami.ay@gmail.com>
2025-10-06 10:46:24 -0300

    start

---
+++ trello_board/src/components/ThemeSwitcher.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import { useDark, useToggle } from '@vueuse/core';
+
+    const isDark = useDark(); // Automatically detects and persists dark mode preference
+    const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <button @click="toggleDark">Toggle Dark Mode</button>
+  <div :class="{ 'dark-theme': isDark, 'light-theme': !isDark }">
+    This content will change theme.
+  </div>
+</template>
+
+<style>
+    .dark-theme {
+        background-color: #333;
+        color: white;
+    }
+    .light-theme {
+        background-color: white;
+        color: #333;
+    }
+</style>
---
+++ trello_board/src/components/TodoForm.vue
@@ -0,0 +1,50 @@
+<script setup>
+    import { ref } from 'vue'
+    
+    function newTask(){
+
+    }    
+</script>
+<template>
+    <div class="form-todo">
+        <form @submit.prevent="newTask">
+            <label>Title todo</label><br>
+            <input 
+                v-model="title_todo"
+                type="text">
+            <br>
+            <label>Description todo</label><br>
+            <textarea 
+                v-model="title_todo"
+                name=""
+                id="">
+            </textarea>
+            <br>
+            <label>Completed</label><br>
+            <input 
+                v-model="completed"
+                type="radio"
+                :value="true"> Yes
+                
+            <input 
+                v-model="completed"
+                type="radio"
+                :value="false"> No
+
+            <br><br>
+
+           <button type="submit">Adicionar</button>
+        </form>
+    </div>
+</template>
+<style scoped>
+    .form-todo {
+        background-color: #3b3a3a;
+        padding: 20px;
+        max-width: 400px;
+        margin: 0 auto;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0,0,0,0.3);
+    }
+
+</style>
